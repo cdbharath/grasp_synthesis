f2272b31772716eb375c1c2e277ce905a6224bdb
UthiraS <usivaraman@wpi.edu>
2023-04-02 20:02:20 -0400

    Code Cleaned (Top Surfce)

--- top_surface_algo/scripts/grasp_synthesis.cpp
+++ top_surface_algo/scripts/grasp_synthesis.cpp
@@ -1,10 +1,10 @@
+// Including all the header files
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/LinearMath/Matrix3x3.h"
 #include <iostream>
 #include <cmath>
-
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/ModelCoefficients.h>
@@ -26,28 +26,22 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/project_inliers.h>
 #include <pcl/surface/concave_hull.h>
-
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <geometry_msgs/Point.h>
 #include <top_surface_algo/GraspPrediction.h>
 
-
+// Defining the class for point cloud
 class PtCloudClass{
   public:
     PtCloudClass(ros::NodeHandle& nh) : n(nh){
         pub = n.advertise<sensor_msgs::PointCloud2>("filtered_cloud", 3);
-        // markers_pub = n.advertise<visualization_msgs::Marker>("grasp_points", 3);
         ros::ServiceServer service = n.advertiseService("coords_in_cam", &PtCloudClass::getGrasp, this);
         cloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
-        // viewer = pcl::visualization::PCLVisualizer::Ptr(new pcl::visualization::PCLVisualizer ("Object Clusters Top Surfaces"));
-        // hullViewer = pcl::visualization::PCLVisualizer::Ptr(new pcl::visualization::PCLVisualizer ("2D Concave hulls"));
-        // graspViewer = pcl::visualization::PCLVisualizer::Ptr(new pcl::visualization::PCLVisualizer ("Grasps"));
-        pt_cloud_sub = n.subscribe<sensor_msgs::PointCloud2>("/camera/depth/color/points", 5, &PtCloudClass::ptCloudCallback, this);
-        // initializeViewers();
+        pt_cloud_sub = n.subscribe<sensor_msgs::PointCloud2>("/camera/depth/color/roi_points", 5, &PtCloudClass::ptCloudCallback, this);
         ros::spin();
     }
-
+    // Callback function to Obtain objectclusters, filtered clouds, Concave hull and grasp points
     void ptCloudCallback(const sensor_msgs::PointCloud2ConstPtr& in_cloud);
     bool getGrasp(top_surface_algo::GraspPrediction::Request  &req, top_surface_algo::GraspPrediction::Response &res);
     pcl::PointCloud<pcl::PointXYZ>::Ptr filter_roi(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
@@ -58,7 +52,7 @@ class PtCloudClass{
     std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> getGrasp(std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds);
     void addCentroid(pcl::PointCloud<pcl::PointXYZ>::Ptr CloudPtr);
     void addMarker(int id, float point1_x, float point1_y, float point1_z, float point2_x, float point2_y, float point2_z);
-    // void initializeViewers();
+    
     
   private:
     ros::NodeHandle n;
@@ -66,28 +60,23 @@ class PtCloudClass{
     ros::Publisher pub;
 
     double centroid_table_z;
-
-    // ros::Publisher markers_pub;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
     pcl::visualization::PCLVisualizer::Ptr viewer;
     pcl::visualization::PCLVisualizer::Ptr hullViewer;
     pcl::visualization::PCLVisualizer::Ptr graspViewer;
-    // visualization_msgs::Marker marker;
+    
 };
-
+//Service for requesting object point cloud 
 bool PtCloudClass::getGrasp(top_surface_algo::GraspPrediction::Request  &req, top_surface_algo::GraspPrediction::Response &res)
 {
     ROS_INFO("Request Recieved");
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_roi = filter_roi(cloud);
-    
-    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> obj_clusters = getObjectClusters(cloud_roi);
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> obj_clusters = getObjectClusters(cloud);
     if (obj_clusters.size() < 1)
     {
         return false;
     }
 
-    // std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> obj_clusters{cloud_roi};
     pcl::PointCloud<pcl::PointXYZRGB> finalCloud;
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> passthroughClusters = getPassthroughFilteredClouds(obj_clusters);
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> convexHulls = getConvexHulls(passthroughClusters);
@@ -98,9 +87,10 @@ bool PtCloudClass::getGrasp(top_surface_algo::GraspPrediction::Request  &req, to
             finalCloud += *graspClouds[i];
         }
     }
+    // Calculating the pose of the Object
     res.best_grasp.pose.position.x = finalCloud[finalCloud.size()-1].x;
     res.best_grasp.pose.position.y = finalCloud[finalCloud.size()-1].y;
-    res.best_grasp.pose.position.z = finalCloud[finalCloud.size()-1].z + 0.01;
+    res.best_grasp.pose.position.z = finalCloud[finalCloud.size()-1].z + 0.015;
 
     Eigen::Vector3f point1(finalCloud[finalCloud.size()-2].x, finalCloud[finalCloud.size()-2].y, finalCloud[finalCloud.size()-2].z);
     Eigen::Vector3f point2(finalCloud[finalCloud.size()-3].x, finalCloud[finalCloud.size()-3].y, finalCloud[finalCloud.size()-3].z);
@@ -108,7 +98,7 @@ bool PtCloudClass::getGrasp(top_surface_algo::GraspPrediction::Request  &req, to
     
     tf2::Quaternion quat;
     quat.setRPY(0, 0, angle + 1.57);
-    //res.best_grasp.pose.orientation.w = 1;    
+    // Calculating the Orientation of the Object
     res.best_grasp.pose.orientation.w = quat.w();    
     res.best_grasp.pose.orientation.x = quat.x();    
     res.best_grasp.pose.orientation.y = quat.y();    
@@ -122,18 +112,6 @@ bool PtCloudClass::getGrasp(top_surface_algo::GraspPrediction::Request  &req, to
     return true;
 }
 
-// void PtCloudClass::initializeViewers(){
-//     viewer->setBackgroundColor (0, 0, 0);
-//     viewer->addCoordinateSystem (0.1);
-//     viewer->initCameraParameters ();
-//     hullViewer->setBackgroundColor (0, 0, 0);
-//     hullViewer->addCoordinateSystem (0.1);
-//     hullViewer->initCameraParameters ();
-//     graspViewer->setBackgroundColor (0, 0, 0);
-//     graspViewer->addCoordinateSystem (0.1);
-//     graspViewer->initCameraParameters ();
-// }
-
 void PtCloudClass::addCentroid(pcl::PointCloud<pcl::PointXYZ>::Ptr CloudPtr){
     Eigen::Matrix< float, 4, 1 > centroid;
     pcl::PointXYZ centroidpoint;
@@ -146,54 +124,12 @@ void PtCloudClass::addCentroid(pcl::PointCloud<pcl::PointXYZ>::Ptr CloudPtr){
     CloudPtr->push_back(centroidpoint);
 }
 
-// void PtCloudClass::addMarker(int id, float point1_x, float point1_y, float point1_z, float point2_x, float point2_y, float point2_z){
-//     visualization_msgs::Marker marker;
-//     marker.header.frame_id = "panda_camera_optical_link";
-//     marker.header.stamp = ros::Time();
-//     marker.id = 0;
-//     marker.type = visualization_msgs::Marker::POINTS;
-//     marker.action = visualization_msgs::Marker::ADD;
-//     marker.ns = "object";
-//     marker.scale.x = 0.01;
-//     marker.scale.y = 0.01;
-//     marker.scale.z = 0.01;
-//     marker.color.g = 1.0f;
-//     marker.color.a = 1.0;
-//     geometry_msgs::Point point1, point2;
-//     point1.x = point1_x;
-//     point1.y = point1_y;
-//     point1.z = point1_z;
-//     point2.x = point2_x;
-//     point2.y = point2_y;
-//     point2.z = point2_z;
-//     marker.points.push_back(point1);
-//     marker.points.push_back(point2);
-//     markers_pub.publish(marker);
-//     // marker_array.markers.push_back(marker);
-// }
-
 void PtCloudClass::ptCloudCallback(const sensor_msgs::PointCloud2ConstPtr& in_cloud){
-    // std::cout << "---------------------------------------------" << std::endl;
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    // sensor_msgs::PointCloud2 output;
+    
     pcl::fromROSMsg(*in_cloud, *cloud);
-    // std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> obj_clusters = getObjectClusters(cloud);
-    // pcl::PointCloud<pcl::PointXYZRGB> finalCloud;
-    // std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> passthroughClusters = getPassthroughFilteredClouds(obj_clusters);
-    // std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> convexHulls = getConvexHulls(passthroughClusters);
-    // std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> graspClouds = getGrasp(convexHulls);
-    // if (convexHulls.size() > 0){
-    //     finalCloud = *graspClouds[0];
-    //     for (std::size_t i = 1; i < graspClouds.size(); i++){
-    //         finalCloud += *graspClouds[i];
-    //     }
-    // }
-    // pcl::toROSMsg(finalCloud, output);
-    // output.header.frame_id = "panda_camera_optical_link";
-    // output.header.stamp = ros::Time::now();
-    // pub.publish(output);
+   
 }
-
+// Function to Segment the object cloud from the table plane via RANSAC 
 std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> PtCloudClass::getObjectClusters(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> object_clusters;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
@@ -253,86 +189,20 @@ std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> PtCloudClass::getObjectClusters
     std::cout << centroid_table[2] << std::endl;
     centroid_table_z = centroid_table[2]; 
 
-    // Creating the KdTree object for the search method of the extraction
-    // pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
-    // tree->setInputCloud (cloud_filtered);
 
-    // std::vector<pcl::PointIndices> cluster_indices;
-    // pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-    // ec.setClusterTolerance (0.02); // 2cm
-    // ec.setMinClusterSize (100);
-    // ec.setMaxClusterSize (25000);
-    // ec.setSearchMethod (tree);
-    // ec.setInputCloud (cloud_filtered);
-    // ec.extract (cluster_indices);
-
-    // int j = 0;
-    // for (const auto& cluster : cluster_indices)
-    // {
-    //     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
-    //     for (const auto& idx : cluster.indices) {
-    //         cloud_cluster->push_back((*cloud_filtered)[idx]);
-    //     }
-    //     cloud_cluster->width = cloud_cluster->size ();
-    //     cloud_cluster->height = 1;
-    //     cloud_cluster->is_dense = true;
-    //     std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size () << " data points." << std::endl;
-    //     object_clusters.push_back(cloud_cluster);
-    //     j++;
-    // }
-    // return object_clusters;
     return {cloud_filtered};
 }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr PtCloudClass::filter_roi(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
-    float min_x = -0.36;
-    float max_x = 0.36;
-    float min_y = -0.22;
-    float max_y = 0.22;
-    float min_z = 0.0;
-    float max_z = 0.7;
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud (cloud);
-    pass.setFilterFieldName ("z");
-    pass.setFilterLimits (min_z, max_z);
-    pass.filter (*cloud_filtered);
-
-    pass.setInputCloud (cloud_filtered);
-    pass.setFilterFieldName ("y");
-    pass.setFilterLimits (min_y, max_y);
-    pass.filter (*cloud_filtered);
-
-    pass.setInputCloud (cloud_filtered);
-    pass.setFilterFieldName ("x");
-    pass.setFilterLimits (min_x, max_x);
-    pass.filter (*cloud_filtered);
-
-    // pcl::visualization::PCLVisualizer roiViewer("ROI Viewer");
-
-    // roiViewer.addPointCloud<pcl::PointXYZ> (cloud_filtered, "cloud_filtered");
-    // roiViewer.setBackgroundColor(0, 0, 0);
-
-    // roiViewer.spinOnce();
-
-    // while(!roiViewer.wasStopped()){
-    //     roiViewer.spinOnce();
-    // }
-
-    return cloud_filtered;
-}
-
+// Function to filter the Z-axis of the object Cluster
 std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> PtCloudClass::getPassthroughFilteredClouds(std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds){
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> fl_clouds;
-    // viewer->removeAllPointClouds();
-    // viewer->removeAllShapes();
+    
     int count = 0;
     for (auto cloud : clouds){
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
         pcl::PointXYZ minPt, maxPt;
         pcl::getMinMax3D (*cloud, minPt, maxPt);
-
+        // Specifying the Z-axis threshold limit for filtering(Please do not change) 
         double z_limit = std::min(0.02, (centroid_table_z + minPt.z)/2 - minPt.z);
 
         pcl::PassThrough<pcl::PointXYZ> pass;
@@ -350,27 +220,20 @@ std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> PtCloudClass::getPassthroughFil
         Eigen::Matrix3f covariance_matrix;
         pcl::compute3DCentroid(*cloud_filtered, pcaCentroid);
 
-        // viewer->addPointCloud<pcl::PointXYZ> (cloud_filtered, "cloud_"+std::to_string(count));
-        // viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud_"+std::to_string(count));
-        // viewer->addSphere(pcl::PointXYZ( pcaCentroid[0], pcaCentroid[1],pcaCentroid[2]), 0.005, 0.5, 0.5, 0.0, "sphere"+std::to_string(count));
-
+       
         fl_clouds.push_back(cloud_filtered);
         count++;
 
-        // while (!viewer->wasStopped ()){
-        //     viewer->spinOnce (100);
-        //     // boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-        // }
+        
     }
-    // viewer->spinOnce (10);
+    
     return fl_clouds;
     
 }
-
+// Function to obtain a Concave Hull 
 std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> PtCloudClass::getConvexHulls(std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds){
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> fl_clouds;
-    // hullViewer->removeAllPointClouds();
-    // hullViewer->removeAllShapes();
+    
     int count = 0;
     for (auto cloud : clouds){
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
@@ -402,13 +265,11 @@ std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> PtCloudClass::getConvexHulls(st
         Eigen::Vector4f pcaCentroid;
         Eigen::Matrix3f covariance_matrix;
         pcl::compute3DCentroid(*cloud_hull, pcaCentroid);
-        // hullViewer->addPointCloud<pcl::PointXYZ> (cloud_hull, "cloud_"+std::to_string(count));
-        // hullViewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud_"+std::to_string(count));
-        // hullViewer->addSphere(pcl::PointXYZ( pcaCentroid[0], pcaCentroid[1],pcaCentroid[2]), 0.005, 0.5, 0.5, 0.0, "sphere"+std::to_string(count));
+        
         fl_clouds.push_back(cloud_hull);
         count++;
     }
-    // hullViewer->spinOnce (10);
+    
     return fl_clouds;
 }
 
@@ -420,17 +281,16 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr PtCloudClass::calculateHull(pcl::PointCloud<
     chull.reconstruct (*cloud_hull);
     return cloud_hull;  
 }
-
+// Obtaining the Grasp Points within the Concave Hull
 std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> PtCloudClass::getGrasp(std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds){
     std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> fl_clouds;
-    // graspViewer->removeAllPointClouds();
-    // graspViewer->removeAllShapes();
+
     int count = 0;
     for (auto CloudPtr : clouds){
         addCentroid(CloudPtr);
         float min_dis = FLT_MAX;
         int index_closest_point,index_opposite_point;
-
+        // Obtaining Grasp Point 1 (GP1) based on nearest eucledian distance from the Centroid
         for (std::size_t i = 0; i < (CloudPtr->points.size() - 1); ++i)
         {
             float dist_x = CloudPtr->points[(CloudPtr->points.size()-1)].x - CloudPtr->points[i].x;
@@ -444,6 +304,7 @@ std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> PtCloudClass::getGrasp(std::
             }
 
         }
+        // Calculating Grasp Point 2 (GP2) which is the opposite point of GP1
         pcl::PointXYZ mirrorpoint;
         mirrorpoint.x = (2*CloudPtr->points[(CloudPtr->points.size()-1)].x) - CloudPtr->points[index_closest_point].x;
         mirrorpoint.y = (2*CloudPtr->points[(CloudPtr->points.size()-1)].y) - CloudPtr->points[index_closest_point].y;
@@ -470,15 +331,15 @@ std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> PtCloudClass::getGrasp(std::
             cloud_vis->points[i].b = 255;
             cloud_vis->points[i].g = 255;
         }
-
+        // Choosing Red color to visualize GP1
         cloud_vis->points[index_closest_point].r = 255;
         cloud_vis->points[index_closest_point].b = 0;
         cloud_vis->points[index_closest_point].g = 0;
-
+        // Choosing Blue color to visualize Centroid
         cloud_vis->points[cloud_vis->points.size()-1].r = 0;
         cloud_vis->points[cloud_vis->points.size()-1].g = 0;
         cloud_vis->points[cloud_vis->points.size()-1].b = 255;
-
+        // Choosing Red color to visualize GP2
         cloud_vis->points[index_opposite_point].r = 255;
         cloud_vis->points[index_opposite_point].b = 0;
         cloud_vis->points[index_opposite_point].g = 0;
@@ -492,16 +353,14 @@ std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> PtCloudClass::getGrasp(std::
         cloud_vis->push_back(cloud_vis->points[index_opposite_point]);
         cloud_vis->push_back(grasppoint);
 
-        // addMarker(count, cloud_vis->points[index_closest_point].x, cloud_vis->points[index_closest_point].y, cloud_vis->points[index_closest_point].z, cloud_vis->points[index_opposite_point].x, cloud_vis->points[index_opposite_point].y, cloud_vis->points[index_opposite_point].z);
-        // graspViewer->addPointCloud<pcl::PointXYZRGB> (cloud_vis, "cloud_"+std::to_string(count));
-        // graspViewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "cloud_"+std::to_string(count));
+        
         fl_clouds.push_back(cloud_vis);
         count++;
     }
-    // graspViewer->spinOnce (10);
+    
     std::cout << "getGrasp finished!" << std::endl;
     return fl_clouds;
-    // return cloud_vis;
+
 }
 
 int main(int argc, char **argv)
